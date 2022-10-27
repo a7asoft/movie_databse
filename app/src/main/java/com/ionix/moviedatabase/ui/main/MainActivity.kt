2b02bea67616fd69ccfaf196cb009355cb761f78
a7asoft <a7asoft@gmail.com>
2022-10-27 12:37:20 -0400

    [feature] Loading indicator and error handling

--- app/src/main/java/com/ionix/moviedatabase/ui/main/MainActivity.kt
+++ app/src/main/java/com/ionix/moviedatabase/ui/main/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.util.TypedValue
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,8 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.coroutineScope
 import androidx.lifecycle.flowWithLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.chip.Chip
+import com.ionix.moviedatabase.R
 import com.ionix.moviedatabase.data.remote.dto.Movie
 import com.ionix.moviedatabase.data.remote.dto.MovieListResponseModel
 import com.ionix.moviedatabase.databinding.ActivityMainBinding
@@ -21,6 +25,7 @@ import kotlinx.coroutines.flow.onEach
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -70,14 +75,58 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleMovies(data: MovieListResponseModel) {
-        Log.wtf("DATA", "$data")
+        binding.swipeLayout.isRefreshing = false
         movieList = data.items as MutableList<Movie>
         //update movies
         binding.rvPopular.adapter?.let { it ->
             if (it is MoviesAdapter) {
-                val ordered = data.items.sortedBy { getDate(it.releaseState) }.distinct().toList().reversed()
+                val ordered =
+                    data.items.sortedBy { getDate(it.releaseState) }.distinct().toList().reversed()
                 it.updateList(ordered)
             }
+        }
+
+        var listGenres = mutableListOf<String>()
+        //resolving genres
+        data.items.forEach { movie ->
+            movie.genreList.forEach { genre ->
+                listGenres.add(genre.value)
+            }
+        }
+
+        listGenres = listGenres.distinct().toMutableList()
+        //building chips dynamically
+        listGenres.forEach { genreString ->
+            val chip = layoutInflater.inflate(R.layout.chip_filter, null, false) as Chip
+            chip.text = genreString
+            val paddingDp = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 10f,
+                resources.displayMetrics
+            ).toInt()
+            chip.setPadding(paddingDp, 0, paddingDp, 0)
+            chip.setOnCheckedChangeListener { b, c ->
+                if (c) {
+                    val filterList =
+                        movieList.filter {
+                            it.genres.lowercase().contains(b.text.toString().lowercase())
+                        }
+                    binding.rvPopular.adapter?.let { rvA ->
+                        if (rvA is MoviesAdapter) {
+                            rvA.updateList(filterList.sortedBy { getDate(it.releaseState) }
+                                .distinct().toList().reversed())
+                        }
+                    }
+                } else {
+                    binding.rvPopular.adapter?.let { rvA ->
+                        if (rvA is MoviesAdapter) {
+                            rvA.updateList(movieList.sortedBy { getDate(it.releaseState) }
+                                .distinct().toList().reversed())
+                        }
+                    }
+                }
+
+            }
+            binding.chipGenres.addView(chip)
         }
     }
 
@@ -107,8 +156,14 @@ class MainActivity : AppCompatActivity() {
                 handleLoading(state.isLoading)
             }
             is MoviesState.ShowError -> {
-                Toast.makeText(this, state.message, Toast.LENGTH_LONG)
-                    .show()
+                binding.errorLayout.visibility = View.VISIBLE
+                binding.searchContainer.visibility = View.GONE
+                binding.textPopular.visibility = View.GONE
+                binding.rvPopular.visibility = View.GONE
+                binding.chipGenres.visibility = View.GONE
+                binding.errorMessage.text = state.message
+                binding.swipeLayout.visibility = View.GONE
+                binding.swipeLayout.isRefreshing = false
             }
             is MoviesState.Init -> {
 
@@ -118,13 +173,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleLoading(loading: Boolean) {
         if (loading) {
-
+            binding.shimmer.visibility = View.VISIBLE
+            binding.errorLayout.visibility = View.GONE
+            binding.chipGenres.visibility = View.GONE
+            binding.textPopular.visibility = View.GONE
+            binding.rvPopular.visibility = View.GONE
         } else {
-
+            binding.shimmer.visibility = View.GONE
+            binding.searchContainer.visibility = View.VISIBLE
+            binding.chipGenres.visibility = View.VISIBLE
+            binding.textPopular.visibility = View.VISIBLE
+            binding.rvPopular.visibility = View.VISIBLE
+            binding.swipeLayout.visibility = View.VISIBLE
         }
     }
 
     private fun listeners() {
+        binding.btnRetry.setOnClickListener {
+            Log.wtf("btnRetry", "Clicked!")
+            observe()
+            viewModel.getMovies()
+        }
 
         binding.etSearch.addTextChangedListener(object : TextWatcher {
 
@@ -137,14 +206,30 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable) {
-                val filterList =
-                    movieList.filter { it.title.lowercase().contains(s.toString().lowercase()) }
-                binding.rvPopular.adapter?.let {
-                    if (it is MoviesAdapter) {
-                        it.updateList(filterList)
+                if (s.toString().isNotEmpty()) {
+                    val filterList =
+                        movieList.filter { it.title.lowercase().contains(s.toString().lowercase()) }
+                    binding.rvPopular.adapter?.let {
+                        if (it is MoviesAdapter) {
+                            it.updateList(filterList.sortedBy { getDate(it.releaseState) }
+                                .distinct().toList().reversed())
+                        }
+                    }
+                } else {
+                    binding.rvPopular.adapter?.let { rvA ->
+                        if (rvA is MoviesAdapter) {
+                            rvA.updateList(movieList.sortedBy { getDate(it.releaseState) }
+                                .distinct().toList().reversed())
+                        }
                     }
                 }
+
             }
         })
+
+        binding.swipeLayout.setOnRefreshListener {
+            observe()
+            viewModel.getMovies()
+        }
     }
 }
